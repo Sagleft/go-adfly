1a198628091e558c2ba92061b626daf10a9c278d
Sagleft <sagleft@mfc.click>
2021-12-11 13:17:36 +0200

    add user ID & api public

--- client.go
+++ client.go
@@ -5,9 +5,15 @@ const (
 )
 
 // Client - ad.fly API client
-type Client struct{}
+type Client struct {
+	UserID       int64
+	APIKeyPublic string
+}
 
 // NewClient - create new ad.fly API client
-func NewClient() *Client {
-	return &Client{}
+func NewClient(userID int64, apiKeyPublic string) *Client {
+	return &Client{
+		UserID:       userID,
+		APIKeyPublic: apiKeyPublic,
+	}
 }
--- shortener.go
+++ shortener.go
@@ -1,12 +1,25 @@
 package goadfly
 
+import (
+	"log"
+	"time"
+)
+
 // ShortenLink - shorten new link
 func (c *Client) ShortenLink(url string) (string, error) {
 	endpoint := apiURL + "shorten"
 	requestData := map[string]interface{}{
-		"url": url,
+		"_user_id":   c.UserID,
+		"_api_key":   c.APIKeyPublic,
+		"_timestamp": time.Now().Unix(),
+		"url":        url,
 	}
-	c.sendPostRequest(endpoint, requestData)
+	response, err := c.sendPostRequest(endpoint, requestData)
+	if err != nil {
+		return "", err
+	}
+
+	log.Println(string(response)) // TEMP
 
 	return "", nil // TODO
 }
