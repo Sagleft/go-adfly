79cd4b04eee4d6ea858e0d9d3e42a10a76f4c06e
Sagleft <sagleft@mfc.click>
2021-12-11 20:43:53 +0200

    remove some fields from structs

--- structs.go
+++ structs.go
@@ -7,13 +7,11 @@ type adflyLinkShortenResponse struct {
 }
 
 type adflyError struct {
-	Code     int64  `json:"code"`
 	Messsage string `json:"msg"`
 	OnURL    string `json:"on"`
 }
 
 type adflyURLResult struct {
-	ID       int64  `json:"id"`
 	URL      string `json:"url"`
 	ShortURL string `json:"short_url"`
 }
